fb726556d91d84502805761f17e4c7f697a7d2d2
Syam <syamgpillai@gmail.com>
2018-03-23 17:02:39 +0530

    My first commit

---
+++ src/Login/login_success.java
@@ -0,0 +1,29 @@
+package Login;
+
+import org.testng.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class login_success {
+	@Test
+	public void testSample1() throws Exception {
+		System.setProperty("webdriver.gecko.driver","D:\\WorkingFolder\\Setups\\Selenium\\geckodriver-v0.19.1-win32\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		
+	  driver.get("http://legalmetrology-qa.rainconcert.biz/login");
+	  driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+    	  driver.findElement(By.id("webmaster")).click();
+    driver.findElement(By.id("email")).click();
+    driver.findElement(By.id("email")).clear();
+    driver.findElement(By.id("email")).sendKeys("boban.gm@rainconcert.in");
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("Rain12345");
+    driver.findElement(By.xpath("//html/body/div/div/div/form/button")).click();
+  }
+  }
+
